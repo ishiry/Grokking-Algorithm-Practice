bd356e60f201ec857ca3fabd4fe76cafdb349663
eiswary <eiswary@pulseid.com>
2019-09-05 21:10:42 +0800

    Update maxArray.py
    chapter 4 change

--- practice/chapter-4/maxArray.py
+++ practice/chapter-4/maxArray.py
@@ -1,1 +1,23 @@
-    #TODO
+#done with some help from online
+
+def max_array(arr):
+    if len(arr) == 0:
+        return 0
+    elif len(arr) == 1:
+        return arr[0]
+    else:
+        maxim = max(arr[0],max_array(arr[1:]))
+        return maxim
+
+
+#test array with length 0
+arr = []
+print(max_array(arr))
+
+#test array with length 1
+arr = [1]
+print(max_array(arr))
+
+#test array with length 1
+arr = [1,2,456546,7]
+print(max_array(arr))
