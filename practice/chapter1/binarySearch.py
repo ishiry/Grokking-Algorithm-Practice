6fec080be4ad177c716e57ce3ef92d5b2de97398
eiswary <eiswary@pulseid.com>
2019-11-01 15:16:49 +0800

    Testing | Run tests

--- practice/chapter1/binarySearch.py
+++ practice/chapter1/binarySearch.py
@@ -9,20 +9,13 @@ def binary_search (list,item):
 
 
     while low <= high:
-        print(step)
         mid = low + high
         guess = list[mid]
         if guess == item:
-            return print('number at index',mid)
+            return mid
         if guess > item:
             high = mid - 1
         else:
             low = mid + 1
         step = step + 1
     return None
-
-#comment
-#its not sensitive to duplicates, as long as the list is in order
-my_list = [1,2,3,4,5,6,7,8,9]
-
-print(binary_search(my_list,5))
--- tests/test_grokking/test_binary_search.py
+++ tests/test_grokking/test_binary_search.py
@@ -2,6 +2,20 @@ import unittest
 import logging.handlers
 
 
-#from grokking_algorithm_practice.practice.chapter1 import binarySearch
+from grokking_algorithm_practice.practice.chapter1.binarySearch import binary_search
 
+class TestBinarySearch(unittest.TestCase):
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+    def test_binary_search_mid(self):
+        result = binary_search(self.my_list, 5)
+        expected = 4
+        self.assertEqual(result, expected)
+
+    def test_binary_search_none(self):
+        result = binary_search(self.my_list, 10)
+        expected = None
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
